66cfbb95fec42557cbc3380f2cfc93610b03ca14
brrusselburg <25828824+brrusselburg@users.noreply.github.com>
2024-02-12 17:41:08 -0600

    Adds sticky styling, app counters

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,17 @@
+require 'date'
+
 class Job < ApplicationRecord
   belongs_to :company
 
   def company
     Company.find(self.company_id)
+  end
+
+  def self.weekly_count
+    Job.where("created_at < ?", 7.days.ago).count
+  end
+
+  def self.daily_count
+    Job.where("created_at = ?", Date.today).count
   end
 end
